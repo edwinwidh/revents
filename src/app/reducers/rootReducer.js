a49639a72275a9306ea4367229f117601850dc67
Edwin Widhardjo <edwin.widh@gmail.com>
2019-10-30 17:20:08 +0700

    sec 9 mid

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer} from 'redux-form'
 import testReducer from '../../features/testarea/testReducer';
 import eventReducers from '../../features/event/eventReducers';
 
 const rootReducer = combineReducers({
+    form: FormReducer,
     test: testReducer,
     events: eventReducers
 })

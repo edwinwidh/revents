62d4f0af4c6157b2b56501011c0fcb413eeb7c8f
Edwin Widhardjo <edwin.widh@gmail.com>
2019-11-01 09:25:33 +0700

    Sec 11

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import { reducer as FormReducer} from 'redux-form'
 import testReducer from '../../features/testarea/testReducer';
 import eventReducers from '../../features/event/eventReducers';
+import modalReducer from '../../features/modals/modalReducer';
+import authReducer from '../../features/auth/authReducer';
 
 const rootReducer = combineReducers({
     form: FormReducer,
     test: testReducer,
-    events: eventReducers
+    events: eventReducers,
+    modals: modalReducer,
+    auth: authReducer
 })
 
 export default rootReducer;
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {incrementCounter, decrementCounter} from './testActions'
+import { incrementCounter, decrementCounter } from "./testActions";
 import { Button } from "semantic-ui-react";
 import TestPlaceInput from "./TestPlaceInput";
 import SimpleMap from "./SimpleMap";
-import {geocodeByAddress, getLatLng} from 'react-places-autocomplete'
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
+import { openModal } from "../modals/modalActions";
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   data: state.test.data
 });
 
 const mapDispatchToProps = {
   incrementCounter,
-  decrementCounter
-}
+  decrementCounter,
+  openModal
+};
 
 class TestComponent extends Component {
-
-  state={
+  state = {
     latlng: {
       lat: 59.95,
       lng: 30.33
     }
-  }
+  };
 
   handleSelect = address => {
     geocodeByAddress(address)
@@ -30,19 +31,24 @@ class TestComponent extends Component {
       .then(latLng => {
         this.setState({
           latlng: latLng
-        })
+        });
       })
-      .catch(error => console.error('Error', error));
+      .catch(error => console.error("Error", error));
   };
 
   render() {
-    const {data, incrementCounter, decrementCounter} = this.props;
+    const { data, incrementCounter, decrementCounter, openModal } = this.props;
     return (
       <div>
         <h1>Test Component</h1>
         <h3>The answer is: {data} </h3>
         <Button onClick={incrementCounter} positive content='Increment' />
         <Button onClick={decrementCounter} negative content='Decrement' />
+        <Button
+          onClick={() => openModal("TestModal", { data: 42 })}
+          color='teal'
+          content='Open Modal'
+        />
         <br />
         <br />
         <TestPlaceInput selectAddress={this.handleSelect} />
@@ -52,4 +58,7 @@ class TestComponent extends Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TestComponent);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TestComponent);

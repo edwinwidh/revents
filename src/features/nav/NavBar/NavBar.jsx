f9705ea241e94a449c2d49a26640bd5035046257
Edwin Widhardjo <edwin.widh@gmail.com>
2019-11-10 12:58:12 +0700

    Sec 15 mid

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,38 +1,38 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { Menu, Container, Button } from "semantic-ui-react";
-import { NavLink, Link, withRouter } from "react-router-dom";
-import SignedOutMenu from "../Menus/SignedOutMenu";
-import SignedInMenu from "../Menus/SignedInMenu";
-import { openModal } from "../../modals/modalActions";
-import { logout } from "../../auth/authActions";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { withFirebase } from 'react-redux-firebase';
+import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink, Link, withRouter } from 'react-router-dom';
+import SignedOutMenu from '../Menus/SignedOutMenu';
+import SignedInMenu from '../Menus/SignedInMenu';
+import { openModal } from '../../modals/modalActions';
 
 const actions = {
-  openModal,
-  logout
+  openModal
 };
 
 const mapState = state => ({
-  auth: state.auth
+  auth: state.firebase.auth,
+  profile: state.firebase.profile
 });
 
 class NavBar extends Component {
   handleSignIn = () => {
-    this.props.openModal("LoginModal");
+    this.props.openModal('LoginModal');
   };
 
   handleRegister = () => {
-    this.props.openModal("RegisterModal");
+    this.props.openModal('RegisterModal');
   };
 
   handleSignOut = () => {
-    this.props.logout();
-    this.props.history.push("/");
+    this.props.firebase.logout();
+    this.props.history.push('/');
   };
 
   render() {
-    const { auth } = this.props;
-    const authenticated = auth.authenticated;
+    const { auth, profile } = this.props;
+    const authenticated = auth.isLoaded && !auth.isEmpty;
     return (
       <Menu inverted fixed='top'>
         <Container>
@@ -60,7 +60,7 @@ class NavBar extends Component {
           {authenticated ? (
             <SignedInMenu
               signOut={this.handleSignOut}
-              currentUser={auth.currentUser}
+              profile={profile}
             />
           ) : (
             <SignedOutMenu
@@ -75,8 +75,10 @@ class NavBar extends Component {
 }
 
 export default withRouter(
-  connect(
-    mapState,
-    actions
-  )(NavBar)
+  withFirebase(
+    connect(
+      mapState,
+      actions
+    )(NavBar)
+  )
 );
--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { Grid } from "semantic-ui-react";
-import SettingsNav from "./SettingsNav";
-import { Route, Redirect, Switch } from "react-router-dom";
-import BasicPage from "./BasicPage";
-import AboutPage from "./AboutPage";
-import PhotosPage from "./PhotosPage";
-import AccountPage from "./AccountPage";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Grid } from 'semantic-ui-react';
+import SettingsNav from './SettingsNav';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import BasicPage from './BasicPage';
+import AboutPage from './AboutPage';
+import PhotosPage from './PhotosPage';
+import AccountPage from './AccountPage';
+import { updatePassword } from '../../auth/authActions';
+
+const actions = {
+  updatePassword
+};
 
 const SettingsDashboard = () => {
   return (
@@ -16,7 +22,10 @@ const SettingsDashboard = () => {
           <Route path='/settings/basic' component={BasicPage} />
           <Route path='/settings/about' component={AboutPage} />
           <Route path='/settings/photos' component={PhotosPage} />
-          <Route path='/settings/account' component={AccountPage} />
+          <Route
+            path='/settings/account'
+            render={() => <AccountPage updatePassword={updatePassword} />}
+          />
         </Switch>
       </Grid.Column>
       <Grid.Column width={4}>
@@ -26,4 +35,7 @@ const SettingsDashboard = () => {
   );
 };
 
-export default SettingsDashboard;
+export default connect(
+  null,
+  actions
+)(SettingsDashboard);
